d65443b8a7886434387f0932700d03781defe4fc
estevao.luna <estevao.luna@cocobambu.com>
2023-03-14 01:48:56 -0300

    added the modal to exclude an substance

--- materias_medicas.js
+++ materias_medicas.js
@@ -99,3 +99,22 @@ document
 document
   .querySelector(".prevButton")
   .addEventListener("click", previousPage, false);
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelectorAll(".btn");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+openModalBtn.forEach((btn) => btn.addEventListener("click", openModal));
+overlay.addEventListener("click", closeModal);
